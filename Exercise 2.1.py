bc32f280655db94e68bafeedda1a6a8c60564c3e
KatarzynaMikruta <161378001+KatarzynaMikruta@users.noreply.github.com>
2024-03-06 15:01:48 +0100

    Add files via upload

---
+++ Exercise 2.1.py
@@ -0,0 +1,7 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=8083084950*904394950
+>>> int(x)
+7310301209201002500
+>>> str(int(x)).count('0')
+8
