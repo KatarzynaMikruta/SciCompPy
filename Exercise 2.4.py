cb4e3a162993886dfcf2e3d3b70f55dce78eb653
KatarzynaMikruta <161378001+KatarzynaMikruta@users.noreply.github.com>
2024-03-06 15:49:10 +0100

    Add files via upload

---
+++ Exercise 2.4.py
@@ -0,0 +1,15 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for char in 'Katarzyna':
+...     print(char, ord(char))
+... 
+...     
+K 75
+a 97
+t 116
+a 97
+r 114
+z 122
+y 121
+n 110
+a 97
